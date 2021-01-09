8ef6a97d4cbce752df0647dd4d8c262e53443efb
JOHN NASIMBA <nasimba4john@gmail.com>
2021-01-09 18:09:48 +0200

    Comparing results

--- script.js
+++ script.js
@@ -38,12 +38,32 @@ function resetSelected() {
   });
 }
 
+
+// Check result, increase scores, update resultText
+function updateScore(playerChoice) {
+  if (playerChoice === computerChoice) {
+    resultText.textContent = "It's a tie.";
+  } else {
+    const choice = choices[playerChoice];
+    if (choice.defeats.indexOf(computerChoice) > -1) {
+
+      resultText.textContent = 'You Won!';
+      playerScoreNumber++;
+      playerScoreEl.textContent = playerScoreNumber;
+    } else {
+      resultText.textContent = 'You Lost!';
+      computerScoreNumber++;
+      computerScoreEl.textContent = computerScoreNumber;
+    }
+  }
+}
+
 // Call functions to process turn
 function checkResult(playerChoice) {
   resetSelected();
   computerRandomChoice();
   displayComputerChoice();
-  
+  updateScore(playerChoice);
 }
 
 
